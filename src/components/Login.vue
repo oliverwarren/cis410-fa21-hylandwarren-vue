05df01ad17a30cbee01cb58e1870660b83512990
Oliver <oliverw@rams.colostate.edu>
2021-12-13 19:02:09 -0700

    15.1 12/13 Vuex, login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,11 +4,81 @@
     <div v-if="this.$route.query.signupsuccess" class="alert alert-success">
       Thanks for signing up, please log in now.
     </div>
+
+    <form @submit.prevent="onSubmit">
+      <div class="mb-3">
+        <label for="email-input" class="form-label">Password</label
+        ><input
+          type="email"
+          class="form-control"
+          id="email-input"
+          required=""
+          placeholder="Enter email"
+          v-model="email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password-input" class="form-label">Password</label
+        ><input
+          type="password"
+          class="form-control"
+          id="password-input"
+          placeholder="Password"
+          required=""
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      <p v-if="credentialsError" class="form-text text-danger">
+        Invalid Credentials
+      </p>
+      <p v-if="loginError" class="form-text text-danger">
+        Could not log you in, please try again later
+      </p>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      loginError: false,
+      credentialsError: false,
+    };
+  },
+  methods: {
+    onSubmit() {
+      let myFormData = {
+        Email: this.email,
+        Password: this.password,
+      };
+      // console.log(myFormData);
+      axios
+        .post("/customers/login", myFormData)
+        .then((myResponse) => {
+          // console.log("the response", myResponse);
+
+          this.$store.commit("storeTokenInApp", myResponse.data.token);
+          this.$store.commit("storeUserInApp", myResponse.data.user);
+
+          this.$router.replace("/account");
+        })
+        .catch((myError) => {
+          console.log("error in /contacts/login", myError);
+
+          if (myError.response.status === 401) {
+            this.credentialsError = true;
+          } else {
+            this.loginError = true;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import axios from "axios";
 
 import myRouter from "./routes.js";
+import theStore from "./store.js";
 
 axios.defaults.baseURL = "https://cis410-rideshare-api.azurewebsites.net/";
 
 const myApp = createApp(App);
 myApp.use(myRouter);
+myApp.use(theStore);
 myApp.mount("#app");
