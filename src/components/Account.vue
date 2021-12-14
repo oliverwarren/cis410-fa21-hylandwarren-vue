7de9b6cda34b619357763ae34393e31fdf465344
Oliver <oliverw@rams.colostate.edu>
2021-12-13 21:53:42 -0700

    15.1 12/13 account

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,11 +1,60 @@
 <template>
   <div>
     <h1>Account</h1>
+    <hr />
+    <h3>{{ firstName }}'s Rides</h3>
+    <p v-if="accountError" class="text-danger">
+      Cannot get your account information, please try again later.
+    </p>
+    <table v-if="ridesByUser" class="table">
+      <thead>
+        <th>Driver</th>
+        <th>Location From</th>
+        <th>Location To</th>
+      </thead>
+      <tbody>
+        <tr v-for="thisRide in ridesByUser" :key="thisRide.RidePK">
+          <th>{{ thisRide.Driver }}</th>
+          <th>{{ thisRide.LocationTo }}</th>
+          <th>{{ thisRide.LocationFrom }}</th>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      ridesByUser: null,
+      accountError: false,
+    };
+  },
+  computed: {
+    firstName() {
+      console.log(("here is the store so far", this.$store.state));
+      return this.$store.state.user.FirstName;
+    },
+  },
+  created() {
+    axios
+      .get("/rides/me", {
+        headers: {
+          Authorization: `Bearer ${this.$store.state.token}`,
+        },
+      })
+      .then((theResponse) => {
+        console.log("here is the response", theResponse);
+        this.ridesByUser = theResponse.data;
+      })
+      .catch(() => {
+        this.accountError = true;
+      });
+  },
+};
 </script>
 
 <style></style>
