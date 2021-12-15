4cef27c8e5778e781f95f1b3a1a6754ba4face0d
Brandon Hyland <bhy@rams.colostate.edu>
2021-12-14 20:22:06 -0700

    rides

--- src/components/Rides.vue
+++ src/components/Rides.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
-    <h1>Rides</h1>
+    <div class="card">
+      <div class="card-body">
+        <h2 class="text-primary">"{{driver.DriverFirstName}} {{driver.DriverLastName}}"</h2>
+        <br/>
+
+        <p>Vehicle Type: <br/><strong>{{driver.VehicleType}}</strong></p>
+      </div>
+    </div>
+    <br/>
+
+
+    <router-link v-if="auth" :to="`/drivers/${this.$route.params.pk}/ride`"><button class="btn btn-success">Add a Ride</button></router-link>
+    <router-link v-else :to="`/login`"><button class="btn btn-outline-success">Sign In to Add a Ride</button></router-link>
+
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed:{
+    driver(){
+      let allDrivers = this.$store.state.drivers
+
+      let thisDriver = allDrivers.find((aDriver)=>{
+        return aDriver.DriverPK == this.$route.params.pk
+      })
+
+      return thisDriver;
+    },
+    // fortmattedBuget(){
+    //   return new Intl.NumberFormat("en-US",{
+    //     style: "currency",
+    //     currency: "USD"
+    //   }).format(this.driver.field_to_format)
+    // }
+    auth(){
+      return this.$store.state.token
+    }
+  },
+};
 </script>
 
 <style></style>
